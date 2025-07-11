b60246e6c050ff1f0e096d0a338fcfbddd870acb
Rohit Dasgupta <rdg922@gmail.com>
2025-07-10 22:41:21 -0400

    Previews

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,22 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Bald Ben",
   description: "Created by Rohit Dasgupta",
+  icons: {
+    icon: "/favicon.ico",
+  },
+  openGraph: {
+    title: "Bald Ben",
+    description: "Created by Rohit Dasgupta",
+    images: [
+      {
+        url: "/preview.png",
+        width: 1200,
+        height: 630,
+        alt: "Bald Ben Preview",
+      },
+    ],
+    type: "website",
+  },
 };
 
 export default function RootLayout({
